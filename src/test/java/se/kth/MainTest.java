f2e642592b9c72712cf1b007dded58537db8956f
Mkiw12 <149476629+Mkiw12@users.noreply.github.com>
2024-04-26 14:55:51 +0200

    Update

---
+++ src/test/java/se/kth/MainTest.java
@@ -0,0 +1,20 @@
+package se.kth;
+
+import org.junit.jupiter.api.Test;
+
+import se.kth.startup.Main;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MainTest {
+
+@Test
+public void startup(){
+
+    String[] args = null;
+    Main.main(args);
+
+}
+    
+}
