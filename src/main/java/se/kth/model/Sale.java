a3260680630d715ef43256454613203cdad704a5
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-04-27 15:59:33 +0200

    ö

--- src/main/java/se/kth/model/Sale.java
+++ src/main/java/se/kth/model/Sale.java
@@ -1,5 +1,20 @@
 package se.kth.model;
 
-public class Sale {
-    
+import java.time.LocalTime;
+
+
+public class Sale 
+{
+    private LocalTime saleTime;
+
+    public Sale()
+    {
+        setTimeOfSale();
+
+    }
+
+    private void setTimeOfSale()
+    {
+        saleTime = LocalTime.now();
+    }
 }
