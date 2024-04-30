d46232fe88e0e1a77bed198e5f37733bb53b6258
Mkiw12 <149476629+Mkiw12@users.noreply.github.com>
2024-04-30 09:39:44 +0200

    updated receipt

--- src/main/java/se/kth/model/Receipt.java
+++ src/main/java/se/kth/model/Receipt.java
@@ -3,4 +3,16 @@ package se.kth.model;
 public class Receipt 
 {
     
+    public Receipt(){
+
+    }
+
+    public createReceipt(float change, SaleDTO updateSalePrice){
+
+    
+        return receipt;
+    } 
+
+
+
 }
