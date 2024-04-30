aaf8ca6c59087073d2f321bcf15cdebf3e806175
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-04-30 10:51:38 +0200

    a

--- src/main/java/se/kth/model/SaleDTO.java
+++ src/main/java/se/kth/model/SaleDTO.java
@@ -1,22 +1,44 @@
 package se.kth.model;
 
 import java.time.LocalTime;
+import java.util.List;
+
+import se.kth.controller.*;
+import se.kth.integration.*;
 
 public class SaleDTO 
 {
     private int saleID;
     private LocalTime time;
-    private float priceTotal;
+    private int priceTotal;
     private float priceAfterDiscount;
     private float vatTotal;
 
-    public SaleDTO(int saleID, LocalTime time, float priceTotal, float priceAfterDiscount, float vatTotal)
+    private List<ItemDTO> registerdItems;
+
+    /*public SaleDTO(int saleID, LocalTime time, float priceTotal, float priceAfterDiscount, float vatTotal)
     {
         this.saleID = saleID;
         this.time = time;
         this.priceTotal = priceTotal;
         this.priceAfterDiscount = priceAfterDiscount;
         this.vatTotal = vatTotal;
+    }
+    */
+    public SaleDTO(List<ItemDTO> registerdItems, int priceTotal)
+    {
+        this.registerdItems = registerdItems;
+        this.priceTotal = priceTotal;
+    }
+
+    public List<ItemDTO> getregisterdItems()
+    {
+        return registerdItems;
+    }
+
+    public int getPriceTotal()
+    {
+        return priceTotal;
     }
 
 
