22f889c1dc5fbb3b1bdda8677c2dde87bb5ecd48
Mkiw12 <149476629+Mkiw12@users.noreply.github.com>
2024-04-30 11:10:53 +0200

    First receipt, fixed SaleDTO

--- src/main/java/se/kth/model/Receipt.java
+++ src/main/java/se/kth/model/Receipt.java
@@ -3,15 +3,39 @@ package se.kth.model;
 public class Receipt 
 {
     
-    public Receipt(){
+    private SaleDTO saleDetails;
+
+    // constructor
+    public Receipt(float change, SaleDTO saleDetails){
+
+        this.saleDetails = saleDetails;
+    }
+
+  
+
+    public SaleDTO getsaleDetails(){
+
+        return saleDetails;
+    }
+
+    public String generateTextReceipt() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("RECEIPT\n");
+        sb.append("-----------------\n");
+        
+        sb.append(saleDetails.getRegisterdItems());
+        sb.append("-----------------\n");
+        sb.append(String.format("Time of sale:",saleDetails.getTime()));
+        sb.append(String.format("Total: %.2f\n", saleDetails.getPriceTotal()));
+        sb.append(String.format("Tax: %.2f\n", saleDetails.getVatTotal()));
+        sb.append(String.format("Change: %.2f\n",saleDetails.getChange()));
+        sb.append("-----------------\n");
+        sb.append("Thank you for shopping!\n");
+        return sb.toString();
 
     }
 
-    public createReceipt(float change, SaleDTO updateSalePrice){
-
-    
-        return receipt;
-    } 
 
 
 
