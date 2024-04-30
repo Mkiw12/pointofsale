3772d4cea4b781f6d89109150f53a1a087d0fc07
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-04-30 09:11:46 +0200

    a

--- src/main/java/se/kth/model/ItemDTO.java
+++ src/main/java/se/kth/model/ItemDTO.java
@@ -7,13 +7,30 @@ public class ItemDTO
     private int itemID;
     private float itemPrice;
 
-    public ItemDTO(String itemName, float itemPrice, int itemID, float itemVat)
+    public ItemDTO(String itemName, float itemPrice, int itemID, double d)
     {
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.itemID = itemID;
         this.itemVat = itemVat;
     }
+
+    public String getItemName(){
+        return itemName;
+    }
+    
+    public int getItemId(){
+        return itemID;
+    }
+    
+    public float getPrice(){
+        return itemPrice;
+    }
+    
+    public float getVAT(){
+        return itemVat;
+    }
+    
 
     
 }
