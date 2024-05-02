eec0792ac671e18897beada88d4c558767f0b40a
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-05-02 13:58:04 +0200

    remove import

--- src/main/java/se/kth/startup/Main.java
+++ src/main/java/se/kth/startup/Main.java
@@ -3,18 +3,13 @@ package se.kth.startup;
 
 import se.kth.controller.Controller;
 import se.kth.view.*;
-import se.kth.integration.*;
-import se.kth.model.Payment;
 
 /**
  * Main class for starting the application. This class initializes all necessary components of the system,
  * including the inventory, accounting, sales log, and the controller, and then starts the user interface.
  */
 public class Main {
-    private InventorySystem inventory;
-    private AccountingSystem accounting;
-    private SalesLog log;
-    private Payment pay;
+
 
     /**
      * The main method that serves as the entry point of the application. Initializes the system components
