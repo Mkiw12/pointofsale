c97b11f7d5bc19e6a10ba93db591f458ca8234f5
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-05-02 10:44:21 +0200

    kommentering

--- src/main/java/se/kth/integration/SalesLog.java
+++ src/main/java/se/kth/integration/SalesLog.java
@@ -4,6 +4,12 @@ import se.kth.model.ShoppingCart;
 
 public class SalesLog 
 {
+    /**
+     * Saves the sold items into the SalesLog to documnet what items has been bought.
+     * The Saleslog contains the list from the cart
+     *
+     * @param cart the ShoppingCart containing the items and quantities purchased
+     */
     public void saveSaleInfo(ShoppingCart cart)
     {
         
